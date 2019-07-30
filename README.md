f040cfdbdc223dbefe55b71d28db61db2fd04074
Amit Kumar <amit.kumar@infoworks.io>
2019-07-31 00:54:40 +0530

    component created

---
+++ README.md
@@ -0,0 +1,14 @@
+# recaptcha-hook ![Language](https://img.shields.io/npm/v/recaptcha-hook)
+
+[`recaptcha-hook`](https://www.npmjs.com/package/recaptcha-hook) is a JavaScript library that lets you easily integrate google recaptcha. `recaptcha-hook` uses react hook to create recaptcha component.
+
+## Installation
+
+Using [npm](https://www.npmjs.com/):
+
+    $ npm install --save recaptcha-hook
+
+```js
+// using ES6 modules
+import ReCaptcha from 'recaptcha-hook';
+```
---
+++ src/index.js
@@ -0,0 +1,106 @@
+import React, {useEffect, useState} from 'react';
+import PropTypes from 'prop-types';
+
+const isReady = () => typeof window !== 'undefined'
+    && typeof window.grecaptcha !== 'undefined'
+    && typeof window.grecaptcha.render === 'function';
+
+let readyCheck;
+
+function ReCaptcha({
+                       siteKey, className = 'g-recaptcha',
+                       elementID = 'g-recaptcha',
+                       onloadCallback = undefined,
+                       onloadCallbackName = 'onloadCallback',
+                       verifyCallback = undefined,
+                       verifyCallbackName = 'verifyCallback',
+                       expiredCallback = undefined,
+                       expiredCallbackName = 'expiredCallback',
+                       render = 'explicit', theme = 'light',
+                       type = 'image', recaptchaVariant = 'normal',
+                       tabindex = '0', badge = 'bottomright'
+                   }) {
+
+    const [ready, setReady] = useState(isReady());
+    let recaptchaWidget;
+    if (!ready && typeof window !== 'undefined') {
+        readyCheck = setInterval(_updateReadyState, 1000);
+    }
+    console.log(ready);
+
+    useEffect(() => {
+        console.log("will mount and update");
+        if (render === 'explicit' && onloadCallback && ready) {
+            loadRecaptcha();
+        }
+        return () => {
+            clearInterval(readyCheck);
+            console.log('will unmount');
+        }
+    }, [loadRecaptcha]);
+
+    function _updateReadyState() {
+        if (isReady()) {
+            setReady(true);
+            clearInterval(readyCheck);
+        }
+    }
+
+    function loadRecaptcha() {
+
+        recaptchaWidget = window.grecaptcha.render(elementID, {
+            sitekey: siteKey,
+            callback: (verifyCallback) ? verifyCallback : undefined,
+            theme: theme,
+            type: type,
+            recaptchaVariant: recaptchaVariant,
+            tabindex: tabindex,
+            badge: badge,
+            'expired-callback': (expiredCallback) ? expiredCallback : undefined,
+        });
+        if (onloadCallback) {
+            onloadCallback();
+        }
+    }
+
+    if (render === 'explicit' && onloadCallback) {
+        return (
+            <div id={elementID}
+                 data-onloadcallbackname={onloadCallbackName}
+                 data-verifycallbackname={verifyCallbackName}
+            />
+        );
+    }
+    return (
+        <div
+            id={elementID}
+            data-site-key={siteKey}
+            data-theme={theme}
+            data-variant={recaptchaVariant}
+            data-badge={badge}
+            className={className}
+            data-tabindex={tabindex}
+        />
+    );
+}
+
+ReCaptcha.propTypes = {
+    className: PropTypes.string,
+    elementID: PropTypes.string,
+    onloadCallbackName: PropTypes.string,
+    verifyCallbackName: PropTypes.string,
+    expiredCallbackName: PropTypes.string,
+    onloadCallback: PropTypes.func,
+    verifyCallback: PropTypes.func,
+    expiredCallback: PropTypes.func,
+    siteKey: PropTypes.string,
+    type: PropTypes.string,
+    tabindex: PropTypes.string,
+    render: PropTypes.oneOf(['onload', 'explicit']),
+    recaptchaVariant: PropTypes.oneOf(['invisible', 'compact', 'normal']),
+    theme: PropTypes.oneOf(['light', 'dark']),
+    badge: PropTypes.oneOf(['bottomright', 'bottomleft', 'inline'])
+};
+
+
+export default ReCaptcha;
